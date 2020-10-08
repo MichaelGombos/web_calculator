6d2f05586f7cbc3764d6942481b3396b28a29ed2
michael gombos <gombos2002@gmail.com>
2020-10-07 23:54:26 -0500

    update calculator logic, fully functional on command line.

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ let storedNumber = 0.0;
 let currentLine = [];
 let currentNumberString = '';
 let currentNumber = 0;
+let displayTestNum = 0;
 const operators = ['+','-','/','X',];
 const commands = ['=','C'];
 
@@ -20,9 +21,9 @@ function divide(num1,num2){ //TO-DO catch /0 error!!!
     return num1/num2;
 }
 
-function operate(operator, num1, num2){
+function operate(num1, operator, num2){
     if(operator == '+'){
-        return add(num1,num2);
+        return add(+num1,+num2);
     }
     if(operator == '-'){
         return subtract(num1,num2);
@@ -42,21 +43,44 @@ function userInput(input){
 
         currentLine.push(currentNumber);
         currentLine.push(input);
-        //adds currentNumber, adds input, moves on!
+        //adds currentNumber, adds input to current working line, moves on!
     }
     else if(commands.includes(input)){
         if(input == '='){ //read EVERYTHING
+            currentNumber = currentNumberString; //Set number int
+            currentNumberString = ''; //clear number String
+    
+            currentLine.push(currentNumber);
+            displayTestNum = solveCurrentLine(currentLine);
 
+            clear();
         }
         else{ //input == C
-            currentNumberString = '';
-            currentNumber = 0;
-            currentLine = [];
+            clear();
         }
     }
     else{
         currentNumberString += input; //adding number to current number
     }
+}
+function clear(){
+    currentNumberString = '';
+    currentNumber = 0;
+    currentLine = [];
+}
+function solveCurrentLine(line){
+    let answer = 0;
+    
+    while(line.length != 1){
+        answer = operate(line[0],line[1],line[2]);
+        line.shift();
+        line.shift();
+    
+        line[0]= answer;
+    }
+    console.log(answer);
+    return answer;
+
 }
 
 
